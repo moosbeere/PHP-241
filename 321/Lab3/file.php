372fcd65baa2f42554e4ef60b8121b0c43d39df0
moosbeere <moosbeere_O@mail.ru>
2025-04-01 11:27:05 +0300

    Lab3 finish

--- 321/Lab3/file.php
+++ 321/Lab3/file.php
@@ -3,5 +3,32 @@
     // var_dump($_SERVER);
     // require('/1/text.txt');
 
-    echo __FILE__.'<BR>';
-    echo __DIR__.'<BR>';
+    // echo __FILE__.'<BR>';
+    // echo __DIR__.'<BR>';
+
+    // file_put_contents('1/text2.txt', '1 2 3 4');
+
+    // $text = file_get_contents('1/text.txt');
+    // echo $text;
+    // file_put_contents('1/text.txt', $text.'!');
+
+
+    // rename('form.php', 'form1.php');
+    // copy('eval.php', 'copy.php');
+    // unlink('eval.php');
+
+    // var_dump(file_exists('1/text3.txt'));
+    // echo filesize('1/text2.txt');
+
+    if (file_exists('1/text.txt')){
+        $text = file_get_contents('1/text.txt');
+        $arr = explode("\n", $text);// explode(PHP_EOL, $text)
+        print_r($arr);
+        echo '<BR>';
+    }
+
+    if (file_exists('1/text.txt')){
+        $text = file('1/text.txt', FILE_IGNORE_NEW_LINES);//array(6) { [0]=> string(1) "1" [1]=> string(1) "2" [2]=> string(1) "2" [3]=> string(1) "3" [4]=> string(1) "4" [5]=> string(1) "5" }
+        // $text = file('1/text.txt');// array(6) { [0]=> string(3) "1 " [1]=> string(3) "2 " [2]=> string(3) "2 " [3]=> string(3) "3 " [4]=> string(3) "4 " [5]=> string(1) "5" }
+        var_dump($text);
+    }
--- 321/Lab3/index.php
+++ 321/Lab3/index.php
@@ -1,61 +1,7 @@
 <?php
-    $a = 4;
-    $b = 3;
+    require('pow2.txt');
+    // include('pow2.txt');
+    $a = 2; $b = 3;
+    $c = pow1($a, $b);
 
-    $s = &$a;
-    // echo "a = $a<BR>";
-    // echo "&s = $s<BR>";
-
-    // $s = 7;
-    // echo "a = $a<BR>";
-
-    // $a = 6;
-    // $z = 'a';
-
-    // echo $$z;//$a
-
-
-    $a = 4; $b = 5; $c = 9;
-
-    list($a, $b, $c) = F1($a, $b, $c);
-    echo "a = $a<BR>";
-    echo "b = $b<BR>";
-    echo "c = $c<BR>";
-
-    function F1($d, $e, $j){
-        $d++; $e--; $j +=$d;
-        return array($d, $e, $j); 
-    }
-
-    function F2(&$d, &$e, &$j){
-        $d++; $e--; $j +=$d;
-    }
-
-    F2($a, $b, $c);
-    echo "a = $a<BR>";
-    echo "b = $b<BR>";
-    echo "c = $c<BR>";
-
-
-    $func = 'sin';
-    $y = 30;
-    $x = $y/180*pi();
-    $z = $func($x);
-    echo $z.'<BR>';
-
-    print ex_val($func,$x)."<BR>";  //1.5
-    print ex_val('sin',$x)."<BR>";  //1.5
-
-    function ex_val($f,$t)
-    {  
-        $x = $f($t)+1;
-        return $x;
-    }
-
-
-    $f ='sin';
-
-    $x=30;
- 
-    eval("\$z=$f($x/180*pi());");
-    echo $z;
+    echo "$b<sup>$a</sup> = $c";
