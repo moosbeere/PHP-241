3b520492a47d9ca62e8fdbb0410aeb20628dfdaa
moosbeere <moosbeere_O@mail.ru>
2025-04-05 10:28:08 +0300

    Lab6

---
+++ 3210_1/Lab6/form.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Session</title>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/css/bootstrap.min.css" integrity="sha384-r4NyP46KrjDleawBgD5tp8Y7UzmLA05oM1iAEQ17CSuDqnUK2+k9luXQOfXJCJ4I" crossorigin="anonymous">
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/js/bootstrap.min.js" integrity="sha384-oesi62hOLfzrys4LxRF63OJCXdXDipiYWBnvTl9Y9/TRlw5xlKIEHpNyvvDShgf/" crossorigin="anonymous"></script>
+</head>
+<body>
+    
+
+<div class="container mt-5">
+        <form action="index.php" method="post">
+            <div class="mb-3">
+                <label class="form-label" for="expression">Enter your expression</label>
+                <input type="text" class="form-control" name="expression" id="expression">
+            </div>
+            <button class="btn btn-primary" type="submit">Calculate</button>
+            <a class="btn btn-warning" href="index.php?session=destroy">Destroy session</a>
+        </form>
+    </div>
+</body>
+</html>
---
+++ 3210_1/Lab6/index.php
@@ -0,0 +1,29 @@
+
+<?php
+    session_start();
+    require('form.php');
+    if(isset($_GET['session'])){
+        session_destroy();
+        $_SESSION['result'] = null;
+    }
+    if(isset($_POST['expression']) && !empty($_POST['expression'])){
+       eval('$result='.$_POST['expression'].';');
+       $key = $_POST['expression'];
+        $_SESSION[$key] = $result;
+    }
+    
+    foreach($_SESSION as $key=>$value)
+        {
+            echo "$key=$value<BR>";
+        }
+
+
+    // session_start();
+    // if (!isset($_SESSION['counter'])) $_SESSION['counter']=1;
+    // else $_SESSION['counter']++;
+    // echo 'Вы обновили страницу: '.$_SESSION['counter'];
+    // session_destroy();
+    // setcookie('test', 'test');
+    // echo $_COOKIE['test'];
+    // setcookie('test', '', time());
+    // echo $_COOKIE['test'];
